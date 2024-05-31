48e55bbf1a72ee0081b23b33680f866ec6a06bcf
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-31 20:46:12 +0000

    Refactor Gameboard class and it's placeShip method
    Implement functionality to check if the ship can
    be placed at the given coordinates

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,35 +1,50 @@
 export default class Gameboard {
-  constructor() {
-    this.grid = Array(10)
+  constructor () {
+    this.board = Array(10)
       .fill(null)
-      .map(() => Array(10).fill(null));
-    this.missedAttacks = [];
-    this.ships = [];
+      .map(() => Array(10).fill(null))
+    this.missedAttacks = []
+    this.ships = []
   }
 
-  placeShip(ship, x, y, orientation) {
-    if (orientation === "horizontal") {
-      for (let i = 0; i < ship.length; i++) {
-        this.grid[x][y + i] = ship;
-      }
-    } else if (orientation === "vertical") {
-      for (let i = 0; i < ship.length; i++) {
-        this.grid[x + i][y] = ship;
+  placeShip (ship, x, y, orientation) {
+    // Check if the ship can be placed at the given coordinates
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === 'horizontal') {
+        if (y + i >= 10 || this.board[x][y + i] !== null) {
+          return false
+        }
+      } else if (orientation === 'vertical') {
+        if (x + i >= 10 || this.board[x + i][y] !== null) {
+          return false
+        }
       }
     }
-    this.ships.push(ship);
+
+    // Place the ship
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === 'horizontal') {
+        this.board[x][y + i] = ship
+      } else if (orientation === 'vertical') {
+        this.board[x + i][y] = ship
+      }
+    }
+    this.ships.push(ship)
+    return true
   }
 
-  receiveAttack(x, y) {
-    const target = this.grid[x][y];
+  receiveAttack (x, y) {
+    const target = this.board[x][y]
     if (target) {
-      target.hit();
+      target.hit()
+      return true
     } else {
-      this.missedAttacks.push([x, y]);
+      this.missedAttacks.push([x, y])
+      return false
     }
   }
 
-  areAllShipsSunk() {
-    return this.ships.every((ship) => ship.isSunk());
+  areAllShipsSunk () {
+    return this.ships.every((ship) => ship.isSunk())
   }
 }
