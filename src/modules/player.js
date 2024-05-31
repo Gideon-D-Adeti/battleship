8e2f28ce4cda8de5a88df422e5a43699eb781292
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-31 20:58:18 +0000

    Update player class and it's test

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,11 +1,12 @@
 import Gameboard from './gameboard'
 
 export default class Player {
-  constructor () {
+  constructor (name) {
+    this.name = name
     this.gameboard = new Gameboard()
   }
 
-  attack (enemyBoard, x, y) {
-    enemyBoard.receiveAttack(x, y)
+  attack (opponent, x, y) {
+    return opponent.gameboard.receiveAttack(x, y)
   }
 }
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,9 +1,8 @@
 import Player from '../modules/player'
-import Gameboard from '../modules/gameboard'
 
 test('Player can attack enemy gameboard', () => {
   const player = new Player()
-  const enemyBoard = new Gameboard()
-  player.attack(enemyBoard, 0, 0)
-  expect(enemyBoard.missedAttacks).toContainEqual([0, 0])
+  const opponent = new Player()
+  player.attack(opponent, 0, 0)
+  expect(opponent.gameboard.missedAttacks).toContainEqual([0, 0])
 })
