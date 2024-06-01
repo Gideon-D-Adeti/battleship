a62ff5175879634a8016ff96ae658a797b673583
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-06-01 08:02:12 +0000

    Implement functionality to render ships on gameboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import './index.css'
+import setupGame from './modules/setupGame'
 import createGameboard from './modules/createGameboard'
+import renderShips from './modules/renderShips'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const { player, computer } = setupGame()
+
   createGameboard('player')
   createGameboard('computer')
+
+  renderShips(player)
+  // renderGameboard(computer)
 })
--- src/modules/createGameboard.js
+++ src/modules/createGameboard.js
@@ -1,5 +1,5 @@
 export default function createGameboard (character) {
-  const gridContainer = document.querySelector(`.${character} .gameboard`)
+  const gameboard = document.querySelector(`.${character} .gameboard`)
 
   // Create column labels
   const columnLabels = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -7,7 +7,7 @@ export default function createGameboard (character) {
     const labelDiv = document.createElement('div')
     labelDiv.textContent = label
     labelDiv.classList.add('cell', 'label')
-    gridContainer.appendChild(labelDiv)
+    gameboard.appendChild(labelDiv)
   })
 
   // Create rows
@@ -16,13 +16,15 @@ export default function createGameboard (character) {
     const rowLabelDiv = document.createElement('div')
     rowLabelDiv.textContent = row
     rowLabelDiv.classList.add('cell', 'label')
-    gridContainer.appendChild(rowLabelDiv)
+    gameboard.appendChild(rowLabelDiv)
 
     // Create cells for the row
     for (let col = 1; col <= 10; col++) {
       const cellDiv = document.createElement('div')
       cellDiv.classList.add('cell')
-      gridContainer.appendChild(cellDiv)
+      cellDiv.dataset.row = row;
+      cellDiv.dataset.col = col;
+      gameboard.appendChild(cellDiv)
     }
   }
 }
