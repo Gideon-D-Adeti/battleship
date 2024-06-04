1c4abc50610c24ad74e5e329dc57cf7e10058575
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-06-04 21:54:58 +0000

    Implement functionality to handle computer moves

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function startGame (player, computer) {
     DOMM.updateNotification("The game started, computer's turn.")
     setTimeout(
       () => handleComputerTurn(player, computer),
-      Math.floor(Math.random() * 6) * 1000
+      Math.random() * (3000 - 1000) + 1000
     )
   }
 
@@ -51,10 +51,10 @@ function startGame (player, computer) {
           DOMM.updateNotification('You won!')
         } else {
           currentPlayer = 'computer'
-          DOMM.updateNotification("Computer's turn.")
+          DOMM.updateNotification("Computer's turn, please wait.")
           setTimeout(
             () => handleComputerTurn(player, computer),
-            Math.floor(Math.random() * 6) * 1000
+            Math.random() * (3000 - 1000) + 1000
           )
         }
       }
@@ -63,5 +63,26 @@ function startGame (player, computer) {
 }
 
 function handleComputerTurn (player, computer) {
-  
+  let x, y
+  let cell
+  do {
+    x = Math.floor(Math.random() * 10)
+    y = Math.floor(Math.random() * 10)
+    cell = document.querySelector(
+      `.player .gameboard .cell[data-row="${x + 1}"][data-col="${y + 1}"]`
+    )
+  } while (cell.classList.contains('attacked'))
+
+  const hit = computer.attack(player, x, y)
+  cell.classList.add('attacked', hit ? 'hit' : 'miss')
+
+  console.table(player.gameboard.board)
+  console.table(player.gameboard.ships)
+
+  if (player.gameboard.areAllShipsSunk()) {
+    DOMM.updateNotification('You lose!')
+  } else {
+    currentPlayer = 'player'
+    DOMM.updateNotification('Your turn.')
+  }
 }
