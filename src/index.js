c4e79f0fb0358570c32780709f77781c67675b76
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-06-04 21:07:48 +0000

    Implement functionality to handle player moves

--- src/index.js
+++ src/index.js
@@ -1,32 +1,67 @@
 import './index.css'
 import setupGame from './modules/setupGame'
-import domManipulation from './modules/domM'
+import DOMManipulation from './modules/DOMM'
+import createBoards from './modules/createBoards'
 
-const dom = new domManipulation()
+const DOMM = new DOMManipulation()
+let currentPlayer
 
 document.addEventListener('DOMContentLoaded', () => {
-  const { player, computer } = setupGame()
+  createBoards()
 
-  player.createBoard()
-  computer.createBoard()
+  const { player, computer } = setupGame()
 
   player.renderBoard()
   computer.renderBoard()
 
-  dom.startButton.addEventListener('click', () => {
+  DOMM.startButton.addEventListener('click', () => {
     startGame(player, computer)
   })
 })
 
 function startGame (player, computer) {
-  dom.unblurComputerBoard()
+  DOMM.unblurComputerBoard()
 
   // Initialize random starter
-  const starter = Math.random() > 0.5 ? 'player' : 'computer'
+  currentPlayer = Math.random() > 0.5 ? 'player' : 'computer'
 
-  if (starter === 'player') {
-    dom.updateNotification('The game started, your turn.')
+  if (currentPlayer === 'player') {
+    DOMM.updateNotification('The game started, your turn.')
   } else {
-    dom.updateNotification("The game started, computer's turn.")
+    DOMM.updateNotification("The game started, computer's turn.")
+    setTimeout(
+      () => handleComputerTurn(player, computer),
+      Math.floor(Math.random() * 6) * 1000
+    )
   }
+
+  // Event listener for player attacks
+  DOMM.computerBoard.addEventListener('click', (event) => {
+    if (currentPlayer === 'player') {
+      const cell = event.target
+      if (
+        cell.classList.contains('cell') &&
+        !cell.classList.contains('attacked')
+      ) {
+        const x = parseInt(cell.dataset.row, 10) - 1
+        const y = parseInt(cell.dataset.col, 10) - 1
+        const hit = player.attack(computer, x, y)
+        cell.classList.add('attacked', hit ? 'hit' : 'miss')
+        if (computer.gameboard.areAllShipsSunk()) {
+          DOMM.updateNotification('You won!')
+        } else {
+          currentPlayer = 'computer'
+          DOMM.updateNotification("Computer's turn.")
+          setTimeout(
+            () => handleComputerTurn(player, computer),
+            Math.floor(Math.random() * 6) * 1000
+          )
+        }
+      }
+    }
+  })
+}
+
+function handleComputerTurn (player, computer) {
+  
 }
